4dd67361a50bf459e6294f6ed074a3c7a3170ae3
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2018-10-13 19:50:10 -0500

    Corrigiendo mis pendejadas :v

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+    <title>Lámina de Tehuacán S.A. de C.V.</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container menu">
+            <figure class="logo">
+                <img src="./assets/logo.jpg" alt="Logo">
+            </figure>
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-linkCont">
+                        <a href="./index.php" class="nav-link">Inicio</a>
+                    </li>
+                    <li class="nav-linkCont">
+                        <a href="./views/products.php" class="nav-link">Productos</a>
+                    </li>
+                    <li class="nav-linkCont">
+                        <a href="./views/services.php" class="nav-link">Servicios</a>
+                    </li>
+                    <li class="nav-linkCont">
+                        <a href="./views/aboutus.php" class="nav-link">Conózcanos</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class = "jumbotron">
+        <div class="container">
+            <div class="jumbo-title">
+                <h1>Bienvenido</h1>
+                <p class="jumbo-description">Somos una empresa orgullosamente mexicana, contamos con más de 10 años de experiencia en el corte, doblado y ensamblaje de estructuras de chapa y rendimiento de fabricación, reacondicionamiento y mantenimiento de partes de carrocería y metal para diferentes usos; También contamos con la tecnología y la capacitación de nuestro personal, que nos permiten ofrecerle un producto de alta calidad a un precio competitivo que cumple con los sistemas de seguridad y la funcionalidad que exige el mercado, respaldados con garantía de satisfacción.</p>
+            </div>
+        </div>
+    </section>
+    <section class="description">
+        <div class="container">
+            <div class="description-content">
+                <h2 class="description-title">Nuestros productos</h2>
+                <div class="description-main">
+                    <p class="description-text">Todos nuestros productos tienen una alta calidad y durabilidad, ya que nos esmeramos en que sean seguros, confiables y cumplan con todas las normas establecidas;además de fabricarlos según sus necesidades y requerimientos.Podemos fabricarle: Cajas refrigeradas de cualquier tamaño, Dollys, Jaulas, Cajas secas, Cajas de redilas, Pipas, Góndolas, Plataformas y Low boy, Piezas para Naves Industriales, etc. Visítenos y conozca la amplia gama de productos que podemos crear para usted</p>
+                    <img src="./assets/pic_Volteos_269633_large.jpg" class="description-image" alt="Carro de volteo">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="services">
+        <div class="container ">
+            <h2 class="services-title">Servicios que ofrecemos</h2>
+            <div class="services-container">
+                <article class="service">
+                    <img src="./assets/torno.JPG" alt="" class="service-image">
+                    <div class="service-content">
+                        <h2 class="service-title">Torno</h2>
+                        <p class="service-description">En laminas de Tehuacán S.A. de C.V. ofrecemos un excelente servicio industrial de torno</p>
+                    </div>
+                </article>
+                <article class="service">
+                    <img src="./assets/soldadura.JPG" alt="" class="service-image">
+                    <div class="service-content">
+                        <h2 class="service-title">Soldadura</h2>
+                        <p class="service-description">En laminas de Tehuacán S.A. de C.V. ofrecemos un excelente servicio industrial de soldadura que sin duda lo dejará satisfecho</p>
+                    </div>
+                </article>
+                <article class="service">
+                    <img src="./assets/roladora.JPG" alt="" class="service-image">
+                    <div class="service-content">
+                        <h2 class="service-title">Roladora</h2>
+                        <p class="service-description">En laminas de Tehuacán S.A. de C.V. ofrecemos un excelente servicio industrial de torno</p>
+                    </div>
+                </article>
+                <a href="#" class="services-seeMore">Ver más</a>
+            </div>
+        </div>
+    </section>
+    <section class="clients">
+        <div class="slider">
+
+        </div>        
+    </section>
+    <section class="contacto">
+        <div class="container">
+            <div class="contacto-form">
+                    <h2>Contáctanos</h2>
+                <form action="" class="form-content">
+                    <input type="text" class="form-input" name="nombre" placeholder="Ingrese su(s) nombre(s)">
+                    <input type="text" class="form-input" name="apMaterno" placeholder="Ingrese su apellido materno">
+                    <input type="text" class="form-input" name="apPaterno" placeholder="Ingrese su apellido paterno">
+                    <input type="email" class="form-input" name="email" placeholder="Ingrese su e-mail">
+                    <input type="phone" class="form-input" name="telefono" placeholder="Ingrese su teléfono">
+                </form>
+                <button type="submit" class="form-submit">Enviar</button>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-contentAddress">
+                    <p>Av. Las Américas # 4824 San Vicente Ferrer en Tehuacán, Puebla.</p>
+                </div>
+                <div class="footer-contentPhones">
+                    <p>Teléfono 1: (01 238) 380-2724</p>
+                    <p>Teléfono 2: (01 238) 374-5107</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
